6634cfacb4d95b8ce1d15a6b110d37b2fc9f0acc
Michael S <smamarsello@gmail.com>
2025-01-08 01:18:58 +0300

    ref: fix app.jsx for eslint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import { useEffect } from 'react'
-
-const unused_variable = 0;
 
 function App() {
   const [count, setCount] = useState(0)
